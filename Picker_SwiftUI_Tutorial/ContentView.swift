86bc2952b4b39a3a3e4e0e6e6bef825102a0df80
Seokhyun Kim <wjapp72@gmail.com>
2021-03-05 10:12:11 -0800

    Initial Commit

---
+++ Picker_SwiftUI_Tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Picker_SwiftUI_Tutorial
+//
+//  Created by Seokhyun Kim on 2021-03-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Picker_SwiftUI_Tutorial/Picker_SwiftUI_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Picker_SwiftUI_TutorialApp.swift
+//  Picker_SwiftUI_Tutorial
+//
+//  Created by Seokhyun Kim on 2021-03-05.
+//
+
+import SwiftUI
+
+@main
+struct Picker_SwiftUI_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
